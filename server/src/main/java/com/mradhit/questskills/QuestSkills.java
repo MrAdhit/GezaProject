ad76559e07a8773291fe674c90087dcd397fc86c
MrAdhit <adhityarimba97@gmail.com>
2022-10-14 14:25:46 +0700

    Init Server Create Build Script

---
+++ server/src/main/java/com/mradhit/questskills/QuestSkills.java
@@ -0,0 +1,17 @@
+package com.mradhit.questskills;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuestSkills extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
